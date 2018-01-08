4f5adbf2c82a6d1c8c6474eb24ca87485944260e
Florry <viktor.soderstrom@live.se>
2018-01-08 22:38:59 +0100

    adds docs file and moves TODOs to that file

--- fruster-user-service.js
+++ fruster-user-service.js
@@ -45,6 +45,8 @@ const CreateInitialUserHandler = require("./lib/handlers/CreateInitialUserHandle
 const VerifyEmailAddressHandler = require('./lib/handlers/email-verification/VerifyEmailAddressHandler.js');
 const ResendVerificationEmailHandler = require('./lib/handlers/email-verification/ResendVerificationEmailHandler.js');
 
+const docs = require("./lib/docs");
+
 
 module.exports = {
 
@@ -59,12 +61,13 @@ module.exports = {
 		const userRepo = new UserRepo(db);
 		const initialUserRepo = new InitialUserRepo(db);
 
-		const createInitialUserHandler = new CreateInitialUserHandler(userRepo, initialUserRepo);
-		await createInitialUserHandler.handle();
-
 		// SERVICES
 		const passwordService = new PasswordService(userRepo);
 		const roleService = new RoleService();
+
+		// INITIALS
+		const createInitialUserHandler = new CreateInitialUserHandler(userRepo, initialUserRepo);
+		await createInitialUserHandler.handle();
 
 		// CREATE
 		const createUserHandler = new CreateUserHandler(userRepo, passwordService, roleService);
@@ -101,7 +104,7 @@ module.exports = {
 			requestSchema: constants.schemas.request.CREATE_USER_REQUEST,
 			responseSchema: constants.schemas.response.USER_RESPONSE,
 			permissions: [constants.permissions.ADMIN_ANY],
-			// docs: TODO:
+			docs: docs.http.admin.CREATE_USER,
 			handle: (req) => createUserHandler.handle(req)
 		});
 
@@ -109,7 +112,7 @@ module.exports = {
 			subject: constants.endpoints.http.admin.GET_USERS,
 			permissions: [constants.permissions.ADMIN_ANY],
 			responseSchema: constants.schemas.response.USER_LIST_RESPONSE,
-			// docs: TODO:
+			docs: docs.http.admin.GET_USERS,
 			handle: (req) => getUserHandler.handleHttp(req)
 		});
 
@@ -117,7 +120,7 @@ module.exports = {
 			subject: constants.endpoints.http.admin.GET_USER,
 			responseSchema: constants.schemas.response.USER_RESPONSE,
 			permissions: [constants.permissions.ADMIN_ANY],
-			// docs: TODO:
+			docs: docs.http.admin.GET_USER,
 			handle: (req) => getUserByIdHandler.handleHttp(req)
 		});
 
@@ -126,27 +129,27 @@ module.exports = {
 			requestSchema: constants.schemas.request.UPDATE_USER_HTTP_REQUEST,
 			responseSchema: constants.schemas.response.USER_RESPONSE,
 			permissions: [constants.permissions.ADMIN_ANY],
-			// docs: TODO:
+			docs: docs.http.admin.UPDATE_USER,
 			handle: (req) => updateUserHandler.handleHttp(req)
 		});
 
 		bus.subscribe({
 			subject: constants.endpoints.http.admin.DELETE_USER,
 			permissions: [constants.permissions.ADMIN_ANY],
-			// docs: TODO:
+			docs: docs.http.admin.DELETE_USER,
 			handle: (req) => deleteUserHandler.handleHttp(req)
 		});
 
 		bus.subscribe({
 			subject: constants.endpoints.http.VERIFY_EMAIL,
 			responseSchema: constants.schemas.response.VERIFY_EMAIL_ADDRESS_RESPONSE,
-			// docs: TODO:
+			docs: docs.http.VERIFY_EMAIL,
 			handle: (req) => verifyEmailAddressHandler.handle(req)
 		});
 
 		bus.subscribe({
 			subject: constants.endpoints.http.RESEND_VERIFICATION_EMAIL,
-			// docs: TODO:
+			docs: docs.http.RESEND_VERIFICATION_EMAIL,
 			handle: (req) => resendVerificationEmailHandler.handle(req)
 		});
 
@@ -156,14 +159,14 @@ module.exports = {
 			subject: constants.endpoints.service.CREATE_USER,
 			requestSchema: constants.schemas.request.CREATE_USER_REQUEST,
 			responseSchema: constants.schemas.response.USER_RESPONSE,
-			// docs: TODO:
+			docs: docs.service.CREATE_USER,
 			handle: (req) => createUserHandler.handle(req)
 		});
 
 		bus.subscribe({
 			subject: constants.endpoints.service.GET_USER,
 			responseSchema: constants.schemas.response.USER_LIST_RESPONSE,
-			// docs: TODO:
+			docs: docs.service.GET_USER,
 			handle: (req) => getUserHandler.handle(req)
 		});
 
@@ -171,6 +174,7 @@ module.exports = {
 			subject: constants.endpoints.service.UPDATE_USER,
 			requestSchema: constants.schemas.request.UPDATE_USER_REQUEST,
 			responseSchema: constants.schemas.response.USER_RESPONSE,
+			docs: docs.service.UPDATE_USER,
 			handle: (req) => updateUserHandler.handle(req)
 		});
 
@@ -178,35 +182,35 @@ module.exports = {
 			subject: constants.endpoints.service.VALIDATE_PASSWORD,
 			requestSchema: constants.schemas.request.VALIDATE_PASSWORD_REQUEST,
 			responseSchema: constants.schemas.response.USER_RESPONSE,
-			// docs: TODO:
+			docs: docs.service.VALIDATE_PASSWORD,
 			handle: (req) => validatePasswordHandler.handle(req)
 		});
 
 		bus.subscribe({
 			subject: constants.endpoints.service.UPDATE_PASSWORD,
 			requestSchema: constants.schemas.request.UPDATE_PASSWORD_REQUEST,
-			// docs: TODO:
+			docs: docs.service.UPDATE_PASSWORD,
 			handle: (req) => updatePasswordHandler.handle(req)
 		});
 
 		bus.subscribe({
 			subject: constants.endpoints.service.DELETE_USER,
 			requestSchema: constants.schemas.request.DELETE_USER_REQUEST,
-			// docs: TODO:
+			docs: docs.service.DELETE_USER,
 			handle: (req) => deleteUserHandler.handle(req),
 		});
 
 		bus.subscribe({
 			subject: constants.endpoints.service.ADD_ROLES,
 			requestSchema: constants.schemas.request.ADD_AND_REMOVE_ROLES_REQUEST,
-			// docs: TODO:
+			docs: docs.service.ADD_ROLES,
 			handle: (req) => addRolesHandler.handle(req)
 		});
 
 		bus.subscribe({
 			subject: constants.endpoints.service.REMOVE_ROLES,
 			requestSchema: constants.schemas.request.ADD_AND_REMOVE_ROLES_REQUEST,
-			// docs: TODO:
+			docs: docs.service.REMOVE_ROLES,
 			handle: (req) => removeRolesHandler.handle(req)
 		});
 
@@ -214,27 +218,27 @@ module.exports = {
 			subject: constants.endpoints.service.GET_SCOPES_FOR_ROLES,
 			requestSchema: constants.schemas.request.GET_SCOPES_FOR_ROLES,
 			responseSchema: constants.schemas.response.STRING_ARRAY_RESPONSE,
-			// docs: TODO:
+			docs: docs.service.GET_SCOPES_FOR_ROLES,
 			handle: (req) => getScopesForRolesHandler.handle(req)
 		});
 
 		bus.subscribe({
 			subject: constants.endpoints.service.SET_PASSWORD,
 			requestSchema: constants.schemas.request.SET_PASSWORD_REQUEST,
-			// docs: TODO:
+			docs: docs.service.SET_PASSWORD,
 			handle: (req) => setPasswordHandler.handle(req)
 		});
 
 		bus.subscribe({
 			subject: constants.endpoints.service.VERIFY_EMAIL,
 			responseSchema: constants.schemas.response.VERIFY_EMAIL_ADDRESS_RESPONSE,
-			// docs: TODO:
+			docs: docs.service.VERIFY_EMAIL,
 			handle: (req) => verifyEmailAddressHandler.handle(req)
 		});
 
 		bus.subscribe({
 			subject: constants.endpoints.service.RESEND_VERIFICATION_EMAIL,
-			// docs: TODO:
+			docs: docs.service.RESEND_VERIFICATION_EMAIL,
 			handle: (req) => resendVerificationEmailHandler.handle(req)
 		});
 
