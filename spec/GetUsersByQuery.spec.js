3a7c4f6e902df4b0d41207c10dd336531c7ab2c1
Viktor Söderström <viktor@frostdigital.se>
2018-08-29 16:45:04 +0200

    [FEATURE] Get users by query w/ expand: profile - query (#85)
    * makes it possible to query profile fields using the get-users-by-query endpoint with expand: "profile"
    
    * changes to correct sorting in tests

--- spec/GetUsersByQuery.spec.js
+++ spec/GetUsersByQuery.spec.js
@@ -20,18 +20,14 @@ describe("GetUsersByQueryHandler", () => {
         SpecUtils.resetConfig();
     });
 
-    it("should be able to get users by a simple query", async done => {
+    it("should be possible to get users by a simple query", async done => {
         await insertTestUsers(10);
 
         try {
             const res = await SpecUtils.busRequest({
                 subject: constants.endpoints.service.GET_USERS_BY_QUERY,
                 data: {
-                    query: {
-                        roles: {
-                            $in: ["user"]
-                        }
-                    }
+                    query: { roles: { $in: ["user"] } }
                 }
             });
 
@@ -51,7 +47,7 @@ describe("GetUsersByQueryHandler", () => {
         }
     });
 
-    it("should be able to get users by a query with expanded profile", async done => {
+    it("should be possible to get users by a query with expanded profile", async done => {
         config.userFields = [constants.dataset.REQUIRED_ONLY];
         await createTestUsers(10);
 
@@ -59,15 +55,9 @@ describe("GetUsersByQueryHandler", () => {
             const res = await SpecUtils.busRequest({
                 subject: constants.endpoints.service.GET_USERS_BY_QUERY,
                 data: {
-                    query: {
-                        roles: {
-                            $in: ["user"]
-                        }
-                    },
+                    query: { roles: { $in: ["user"] } },
                     expand: "profile",
-                    sort: {
-                        firstName: 1
-                    }
+                    sort: { "profile.firstName": 1 }
                 }
             });
 
@@ -98,32 +88,40 @@ describe("GetUsersByQueryHandler", () => {
         }
     });
 
-    it("should be able to get users by a query with expanded profile w/ old user data", async done => {
-        await insertTestUsers(5);
+    it("should be possible to get users by a query with expanded profile included in query", async done => {
         config.userFields = [constants.dataset.REQUIRED_ONLY];
-        await createTestUsers(5, 5);
+        await createTestUsers(10);
 
         try {
             const res = await SpecUtils.busRequest({
                 subject: constants.endpoints.service.GET_USERS_BY_QUERY,
                 data: {
                     query: {
-                        roles: {
-                            $in: ["user"]
-                        }
+                        roles: { $in: ["user"] },
+                        "profile.firstName": "user7-firstName"
                     },
                     expand: "profile",
-                    sort: {
-                        firstName: 1
-                    }
+                    sort: { "profile.firstName": 1 }
                 }
             });
 
-            expect(res.data.users.length).toBe(10, "res.data.users.length");
-            expect(res.data.totalCount).toBe(10, "res.data.totalCount");
+            expect(res.data.users.length).toBe(1, "res.data.users.length");
+            expect(res.data.totalCount).toBe(1, "res.data.totalCount");
 
-            expect(res.data.users.filter(u => !!u.profile).length).toBe(5);
-            expect(res.data.users.filter(u => !u.profile).length).toBe(5);
+            expect(res.data.users[0].id).toBeDefined("res.data.users[0].id");
+            expect(res.data.users[0].password).toBeUndefined("res.data.users[0].password");
+            expect(res.data.users[0].salt).toBeUndefined("res.data.users[0].salt");
+            expect(res.data.users[0].profile).toBeDefined("res.data.users[0].profile");
+            expect(res.data.users[0].profile.firstName).toBe(`user7-firstName`, "res.data.users[0].profile.firstName");
+            expect(res.data.users[0].profile.lastName).toBe(`user7-lastName`, "res.data.users[0].profile.lastName");
+            expect(res.data.users[0].profile.customField).toBe(Math.cos(7), "res.data.users[0].profile.customField");
+
+            expect(res.data.users[0].metadata).toBeDefined("res.data.users[0].profile.metadata");
+            expect(res.data.users[0].metadata.created).toBeDefined("res.data.users[0].profile.metadata.created");
+            expect(res.data.users[0].metadata.updated).toBeDefined("res.data.users[0].profile.metadata.updated");
+            expect(res.data.users[0].profile.metadata).toBeDefined("res.data.users[0].profile.metadata");
+            expect(res.data.users[0].profile.metadata.created).toBeDefined("res.data.users[0].profile.metadata.created");
+            expect(res.data.users[0].profile.metadata.updated).toBeDefined("res.data.users[0].profile.metadata.updated");
 
             done();
         } catch (err) {
@@ -132,18 +130,131 @@ describe("GetUsersByQueryHandler", () => {
         }
     });
 
-    it("should be able to paginate result from get users by query  with `limit`", async done => {
-        await insertTestUsers(10);
+    it("should be possible to get users by a query with expanded profile included in query and filter result", async done => {
+        config.userFields = [constants.dataset.REQUIRED_ONLY];
+        await createTestUsers(10);
 
         try {
             const res = await SpecUtils.busRequest({
                 subject: constants.endpoints.service.GET_USERS_BY_QUERY,
                 data: {
                     query: {
-                        roles: {
-                            $in: ["user"]
-                        }
+                        roles: { $in: ["user"] },
+                        "profile.firstName": "user7-firstName"
                     },
+                    expand: "profile",
+                    sort: {
+                        "profile.firstName": 1
+                    },
+                    filter: {
+                        id: 1,
+                        "profile.firstName": 1
+                    }
+                }
+            });
+
+            expect(res.data.users.length).toBe(1, "res.data.users.length");
+            expect(res.data.totalCount).toBe(1, "res.data.totalCount");
+
+            expect(res.data.users[0].id).toBeDefined("res.data.users[0].id");
+            expect(res.data.users[0].password).toBeUndefined("res.data.users[0].password");
+            expect(res.data.users[0].salt).toBeUndefined("res.data.users[0].salt");
+            expect(res.data.users[0].profile).toBeDefined("res.data.users[0].profile");
+            expect(res.data.users[0].profile.firstName).toBe(`user7-firstName`, "res.data.users[0].profile.firstName");
+            expect(res.data.users[0].profile.lastName).toBeUndefined("res.data.users[0].profile.lastName");
+            expect(res.data.users[0].profile.customField).toBeUndefined("res.data.users[0].profile.customField");
+
+            expect(res.data.users[0].metadata).toBeUndefined("res.data.users[0].profile.metadata");
+            expect(res.data.users[0].profile.metadata).toBeUndefined("res.data.users[0].profile.metadata");
+
+            done();
+        } catch (err) {
+            log.error(err);
+            done.fail(err);
+        }
+    });
+
+    it("should be possible to get users by a query with expanded profile included in query and limit result", async done => {
+        config.userFields = [constants.dataset.REQUIRED_ONLY];
+        await createTestUsers(10);
+
+        try {
+            const res = await SpecUtils.busRequest({
+                subject: constants.endpoints.service.GET_USERS_BY_QUERY,
+                data: {
+                    query: {
+                        roles: { $in: ["user"] },
+                        "profile.firstName": { $exists: true }
+                    },
+                    expand: "profile",
+                    sort: { "profile.firstName": 1 },
+                    limit: 2
+                }
+            });
+
+            expect(res.data.users.length).toBe(2, "res.data.users.length");
+            expect(res.data.totalCount).toBe(10, "res.data.totalCount");
+
+            for (let i = 0; i < 2; i++) {
+                expect(res.data.users[i].id).toBeDefined("res.data.users[i].id");
+                expect(res.data.users[i].password).toBeUndefined("res.data.users[i].password");
+                expect(res.data.users[i].salt).toBeUndefined("res.data.users[i].salt");
+                expect(res.data.users[i].profile).toBeDefined("res.data.users[i].profile");
+                expect(res.data.users[i].profile.firstName).toBe(`user${i}-firstName`, "res.data.users[i].profile.firstName");
+                expect(res.data.users[i].profile.lastName).toBe(`user${i}-lastName`, "res.data.users[i].profile.lastName");
+                expect(res.data.users[i].profile.customField).toBe(Math.cos(i), "res.data.users[i].profile.customField");
+
+                expect(res.data.users[i].metadata).toBeDefined("res.data.users[i].profile.metadata");
+                expect(res.data.users[i].metadata.created).toBeDefined("res.data.users[i].profile.metadata.created");
+                expect(res.data.users[i].metadata.updated).toBeDefined("res.data.users[i].profile.metadata.updated");
+                expect(res.data.users[i].profile.metadata).toBeDefined("res.data.users[i].profile.metadata");
+                expect(res.data.users[i].profile.metadata.created).toBeDefined("res.data.users[i].profile.metadata.created");
+                expect(res.data.users[i].profile.metadata.updated).toBeDefined("res.data.users[i].profile.metadata.updated");
+            }
+
+            done();
+        } catch (err) {
+            log.error(err);
+            done.fail(err);
+        }
+    });
+
+    it("should be possible to get users by a query with expanded profile w/ old user data", async done => {
+        await insertTestUsers(5);
+        config.userFields = [constants.dataset.REQUIRED_ONLY];
+        await createTestUsers(5, 5);
+
+        try {
+            const res = await SpecUtils.busRequest({
+                subject: constants.endpoints.service.GET_USERS_BY_QUERY,
+                data: {
+                    query: { roles: { $in: ["user"] } },
+                    expand: "profile",
+                    sort: { "profile.firstName": 1 }
+                }
+            });
+
+            expect(res.data.users.length).toBe(10, "res.data.users.length");
+            expect(res.data.totalCount).toBe(10, "res.data.totalCount");
+
+            expect(res.data.users.filter(u => !!u.profile).length).toBe(5, "users without profile");
+            expect(res.data.users.filter(u => !u.profile).length).toBe(5, "users with profile");
+
+            done();
+        } catch (err) {
+            log.error(err);
+            done.fail(err);
+        }
+    });
+
+    it("should be possible to paginate result from get users by query  with `limit`", async done => {
+        await insertTestUsers(10);
+
+        try {
+            const res = await SpecUtils.busRequest({
+                subject: constants.endpoints.service.GET_USERS_BY_QUERY,
+                data: {
+                    query: { roles: { $in: ["user"] } },
                     limit: 3
                 }
             });
@@ -164,18 +275,14 @@ describe("GetUsersByQueryHandler", () => {
         }
     });
 
-    it("should be able to shift paginated result from get users by query with `start`", async done => {
+    it("should be possible to shift paginated result from get users by query with `start`", async done => {
         await insertTestUsers(10);
 
         try {
             const res = await SpecUtils.busRequest({
                 subject: constants.endpoints.service.GET_USERS_BY_QUERY,
                 data: {
-                    query: {
-                        roles: {
-                            $in: ["user"]
-                        }
-                    },
+                    query: { roles: { $in: ["user"] } },
                     limit: 3,
                     start: 3
                 }
@@ -197,18 +304,14 @@ describe("GetUsersByQueryHandler", () => {
         }
     });
 
-    it("should be able to filter result with `filter`", async done => {
+    it("should be possible to filter result with `filter`", async done => {
         await insertTestUsers(10);
 
         try {
             const res = await SpecUtils.busRequest({
                 subject: constants.endpoints.service.GET_USERS_BY_QUERY,
                 data: {
-                    query: {
-                        roles: {
-                            $in: ["user"]
-                        }
-                    },
+                    query: { roles: { $in: ["user"] } },
                     limit: 3,
                     start: 3,
                     filter: {
@@ -234,7 +337,7 @@ describe("GetUsersByQueryHandler", () => {
         }
     });
 
-    it("should be able to sort result with `sort`", async done => {
+    it("should be possible to sort result with `sort`", async done => {
         await insertTestUsers(10);
 
         try {
@@ -267,11 +370,7 @@ describe("GetUsersByQueryHandler", () => {
             return await await SpecUtils.busRequest({
                 subject: constants.endpoints.service.GET_USERS_BY_QUERY,
                 data: {
-                    query: {
-                        roles: {
-                            $in: ["user"]
-                        }
-                    },
+                    query: { roles: { $in: ["user"] } },
                     limit: 3,
                     start: 3,
                     filter: {
@@ -294,11 +393,7 @@ describe("GetUsersByQueryHandler", () => {
             const res = await SpecUtils.busRequest({
                 subject: constants.endpoints.service.GET_USERS_BY_QUERY,
                 data: {
-                    query: {
-                        roles: {
-                            $in: ["no-one-can-have-this-role"]
-                        }
-                    },
+                    query: { roles: { $in: ["no-one-can-have-this-role"] } },
                     limit: 3,
                     start: 3,
                     filter: {
