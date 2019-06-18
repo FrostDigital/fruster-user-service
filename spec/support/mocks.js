f8acf656fee6aa7d56d6fdccba1476900c29f2cc
Joel Soderstrom <joel.soderstrom@gmail.com>
2019-06-18 16:58:10 +0200

    Updates hash/salt for test

--- spec/support/mocks.js
+++ spec/support/mocks.js
@@ -5,62 +5,62 @@ const SpecUtils = require("./SpecUtils");
 
 module.exports = {
 
-    getUserObject: () => {
-        return {
-            "roles": ["admin"],
-            "firstName": "Viktor",
-            "middleName": "Ludvig",
-            "lastName": "Söderström",
-            "email": uuid.v4() + "@frostdigxital.se",
-            "password": "Localhost:8080",
-            "emailVerified": true
-        };
-    },
+	getUserObject: () => {
+		return {
+			"roles": ["admin"],
+			"firstName": "Viktor",
+			"middleName": "Ludvig",
+			"lastName": "Söderström",
+			"email": uuid.v4() + "@frostdigxital.se",
+			"password": "Localhost:8080",
+			"emailVerified": true
+		};
+	},
 
-    getOldUserObject: () => {
-        return {
-            "email": "admin@frost.se",
-            "password": "4ac754da841d466229c0e4d6e449b2177ab9efdf2c640c55217abc8dd447290746148b1c275dfb4a4c4ecceaf812cb6feb8c6fa5372dd55f56994a93cdcc9c26",
-            "firstName": "admin",
-            "lastName": "nimda",
-            "roles": [
-                "super-admin"
-            ],
-            "middleName": null,
-            "id": "58b6fd6a-f651-4e8a-8297-acf5c0494a58",
-            "salt": "nd0h6fbp0e65sdrsp0lg0izpajko403wou479oef0xvyq5jaif"
-        };
-    },
+	getOldUserObject: () => {
+		return {
+			"email": "admin@frost.se",
+			"password": "b7b5e35f85746c6f0266b8f4b725d0fc35bd594f1aa64c966bac6f9eff5de4f434188df944329ae2eb69448ff8a389719bc5771909f98b9a1de3d77d8fb88ddc",
+			"firstName": "admin",
+			"lastName": "nimda",
+			"roles": [
+				"super-admin"
+			],
+			"middleName": null,
+			"id": "58b6fd6a-f651-4e8a-8297-acf5c0494a58",
+			"salt": "o4oxemd7ybieolg12nk7woozll4fxpqnxqaqr6v9w1ntx4kg55"
+		};
+	},
 
-    getUserWithUnverifiedEmailObject: () => {
-        return {
-            "roles": ["admin"],
-            "firstName": "Pedro",
-            "middleName": "Von",
-            "lastName": "Damn",
-            "email": uuid.v4() + "@frostdigxitalx.se",
-            "password": "Localhost:8080",
-            "emailVerified": false,
-            "emailVerificationToken": "hello-token"
-        };
-    },
+	getUserWithUnverifiedEmailObject: () => {
+		return {
+			"roles": ["admin"],
+			"firstName": "Pedro",
+			"middleName": "Von",
+			"lastName": "Damn",
+			"email": uuid.v4() + "@frostdigxitalx.se",
+			"password": "Localhost:8080",
+			"emailVerified": false,
+			"emailVerificationToken": "hello-token"
+		};
+	},
 
-    createUser: async (userObj) => {
-        return await SpecUtils.busRequest({
-            subject: constants.endpoints.service.CREATE_USER,
-            data: userObj
-        });
-    },
+	createUser: async (userObj) => {
+		return await SpecUtils.busRequest({
+			subject: constants.endpoints.service.CREATE_USER,
+			data: userObj
+		});
+	},
 
-    mockMailService: (expectFunc) => {
-        bus.subscribe("mail-service.send", (req) => {
-            if (expectFunc)
-                expectFunc(req);
-            return {
-                reqId: req.reqId,
-                status: 200
-            }
-        });
-    }
+	mockMailService: (expectFunc) => {
+		bus.subscribe("mail-service.send", (req) => {
+			if (expectFunc)
+				expectFunc(req);
+			return {
+				reqId: req.reqId,
+				status: 200
+			}
+		});
+	}
 
 }
