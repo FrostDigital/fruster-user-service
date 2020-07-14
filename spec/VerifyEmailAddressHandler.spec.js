59360111885c13d3e2412bc6392643056d4d6596
Dinuka Thilanga <dinuka@frostdigital.se>
2020-07-14 14:45:22 +0530

    [DEV] Skip email unique index creation by config and email is not the username (#120)
    [FIX] Typo issue emailVerificationEmailTemplate
    [REF] Update packages
    [REF] Reduce unwanted conditions in ValidatePasswordHandler

--- spec/VerifyEmailAddressHandler.spec.js
+++ spec/VerifyEmailAddressHandler.spec.js
@@ -13,71 +13,71 @@ const SpecUtils = require("./support/SpecUtils");
 
 describe("VerifyEmailAddressHandler", () => {
 
-    /** @type {Db} */
-    let db;
+	/** @type {Db} */
+	let db;
 
-    frusterTestUtils
-        .startBeforeEach(specConstants
-            .testUtilsOptions((connection) => db = connection.db));
+	frusterTestUtils
+		.startBeforeEach(specConstants
+			.testUtilsOptions((connection) => db = connection.db));
 
-    beforeAll(() => config.requireEmailVerification = true);
+	beforeAll(() => config.requireEmailVerification = true);
 
-    afterAll(() => SpecUtils.resetConfig());
+	afterAll(() => SpecUtils.resetConfig());
 
-    afterEach(() => userService.stop());
+	afterEach(() => userService.stop());
 
-    it("should remove emailVerificationToken and set emailVerified to true when verifying with emailVerificationToken", async () => {
-        const testUserData = mocks.getUserWithUnverifiedEmailObject();
+	it("should remove emailVerificationToken and set emailVerified to true when verifying with emailVerificationToken", async () => {
+		const testUserData = mocks.getUserWithUnverifiedEmailObject();
 
-        bus.subscribe(MailServiceClient.endpoints.SEND, (req) => {
-            expect(req.data.from).toBe(config.emailVerificationFrom, "req.data.from");
-            expect(req.data.to.includes(testUserData.email)).toBeTruthy("req.data.to.includes(testUserData.email)");
-            return { reqId: req.reqId, status: 200 }
-        });
+		bus.subscribe(MailServiceClient.endpoints.SEND, (req) => {
+			expect(req.data.from).toBe(config.emailVerificationFrom, "req.data.from");
+			expect(req.data.to.includes(testUserData.email)).toBeTruthy("req.data.to.includes(testUserData.email)");
+			return { reqId: req.reqId, status: 200 }
+		});
 
-        const createUserResponse = (await mocks.createUser(testUserData)).data;
-        const testUser = await db.collection(constants.collections.USERS).findOne({ id: createUserResponse.id });
-        const verificationResponse = await SpecUtils.busRequest({
-            subject: constants.endpoints.http.VERIFY_EMAIL,
-            params: { tokenId: testUser.emailVerificationToken }
-        });
+		const createUserResponse = (await mocks.createUser(testUserData)).data;
+		const testUser = await db.collection(constants.collections.USERS).findOne({ id: createUserResponse.id });
+		const verificationResponse = await SpecUtils.busRequest({
+			subject: constants.endpoints.http.VERIFY_EMAIL,
+			params: { tokenId: testUser.emailVerificationToken }
+		});
 
-        expect(verificationResponse.status).toBe(200, "verificationResponse.status");
+		expect(verificationResponse.status).toBe(200, "verificationResponse.status");
 
-        const updatedTestUser = await db.collection(constants.collections.USERS).findOne({ id: createUserResponse.id });
+		const updatedTestUser = await db.collection(constants.collections.USERS).findOne({ id: createUserResponse.id });
 
-        expect(updatedTestUser.emailVerificationToken).toBeUndefined("should remove emailVerificationToken");
-        expect(updatedTestUser.emailVerified).toBe(true, "should set emailVerified to true");
-    });
+		expect(updatedTestUser.emailVerificationToken).toBeUndefined("should remove emailVerificationToken");
+		expect(updatedTestUser.emailVerified).toBe(true, "should set emailVerified to true");
+	});
 
-    it("should not be able to verify email with faulty token", async done => {
-        try {
-            await SpecUtils.busRequest({
-                subject: constants.endpoints.http.VERIFY_EMAIL,
-                params: { tokenId: "ram.jam" }
-            });
+	it("should not be able to verify email with faulty token", async done => {
+		try {
+			await SpecUtils.busRequest({
+				subject: constants.endpoints.http.VERIFY_EMAIL,
+				params: { tokenId: "ram.jam" }
+			});
 
-            done.fail();
-        } catch (err) {
-            expect(err.error.code).toBe(errors.get("fruster-user-service.INVALID_TOKEN").error.code);
-            done();
-        }
-    });
+			done.fail();
+		} catch (err) {
+			expect(err.error.code).toBe(errors.get("fruster-user-service.INVALID_TOKEN").error.code);
+			done();
+		}
+	});
 
-    it("should use sendgrid mail template if specified in config", async done => {
-        config.emailVerificationEmailTempate = "band-ola";
+	it("should use sendgrid mail template if specified in config", async done => {
+		config.emailVerificationEmailTemplate = "band-ola";
 
-        const testUserData = mocks.getUserWithUnverifiedEmailObject();
+		const testUserData = mocks.getUserWithUnverifiedEmailObject();
 
-        bus.subscribe(MailServiceClient.endpoints.SEND, (req) => {
-            expect(req.data.from).toBe(config.emailVerificationFrom);
-            expect(req.data.to[0]).toBe(testUserData.email);
-            expect(req.data.templateId).toBe(config.emailVerificationEmailTempate);
+		bus.subscribe(MailServiceClient.endpoints.SEND, (req) => {
+			expect(req.data.from).toBe(config.emailVerificationFrom);
+			expect(req.data.to[0]).toBe(testUserData.email);
+			expect(req.data.templateId).toBe(config.emailVerificationEmailTemplate);
 
-            done();
-        });
+			done();
+		});
 
-        await mocks.createUser(testUserData);
-    });
+		await mocks.createUser(testUserData);
+	});
 
 });
